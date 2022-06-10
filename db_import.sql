21f209a68c4217749b171093e9c54e54fd6d9968
LautaroRiveros <lautyriveros73@gmail.com>
2022-06-10 16:13:02 -0300

    adding db backup

---
+++ db_import.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 10, 2022 at 09:12 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `acciones`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `symbols`
+--
+
+CREATE TABLE `symbols` (
+  `Id` int(200) NOT NULL,
+  `Name` varchar(250) NOT NULL,
+  `Symbol` varchar(100) NOT NULL,
+  `userId` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `Id` int(11) NOT NULL,
+  `User` varchar(50) NOT NULL,
+  `Name` varchar(200) NOT NULL,
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`Id`, `User`, `Name`, `Password`) VALUES
+(1, 'originsoftware', 'Origin Software', '$2b$10$KLMuIRlU65TTxr1Kc/PYjeJrS4ZasZDZU5znXtle.GyB7ItfCy24y');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `symbols`
+--
+ALTER TABLE `symbols`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `symbols`
+--
+ALTER TABLE `symbols`
+  MODIFY `Id` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `symbols`
+--
+ALTER TABLE `symbols`
+  ADD CONSTRAINT `symbols_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`Id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
